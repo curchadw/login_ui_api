9a7dc5198d1cbd361923a6bb47fd3e57c09cea9e
curtis <laurencechadwell@gmail.com>
2021-02-24 22:06:48 -0600

    First Commit

---
+++ db/migrate/create_users.rb
@@ -0,0 +1,11 @@
+class CreateSignUps < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :avatar
+      t.timestamps
+    end
+  end
+end
