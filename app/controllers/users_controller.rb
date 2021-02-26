e9d62ce565d638b2534e848dfe5363557e4cc3f1
curtis <laurencechadwell@gmail.com>
2021-02-25 21:19:49 -0600

    Frontend post action issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+    
+    
+    def create
+        new_user = User.new(user_params)
+        if new_user.save
+            token = JsonWebToken.encode(user_id: new_user.id)
+            time = Time.now + 24.hour.to_i
+            render json: {token: token, time: time}, status: :ok
+        else
+            head(:unprocessable_enity)
+        end
+    end
+
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :avatar)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :sign_ups
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  get '/users',  to: 'users#index'
+  post '/users', to: 'users#create'
 end
